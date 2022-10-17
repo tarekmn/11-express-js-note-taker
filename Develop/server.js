0d447f5ffe34780d85ca727a5e1475d917e23e85
tarekmn <nytarek@gmail.com>
2022-10-17 13:09:21 -0500

    updated post

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const data = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 const fs = require('fs');
 
@@ -11,7 +10,7 @@ const PORT = 3001;
 
 //Invoking app.use() and serve static files from '/Develop'
 
-app.use(express.static('develop'));
+app.use(express.static('public'));
 
 // Sets up the Express app to handle data parsing -MAYBE NOT NEEDED SO DOUBLE CHECK
 app.use(express.urlencoded({ extended: true }));
@@ -24,34 +23,36 @@ app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html')));
+res.sendFile(path.join(__dirname, './public/notes.html')));
 
 //API get route to read db.json file and return all saved notes
-app.get('/api/notes', (req, res) => 
-res.json(data)
+app.get('/api/notes', (req, res) => {
+  const filePath = './db/db.json';
+  const file = fs.readFileSync(filePath);
+  const fileContents = file.toString();
+  let jsonResponse = JSON.parse(fileContents);
+
+  console.log(jsonResponse)
+  if (!Array.isArray(jsonResponse)) jsonResponse = [jsonResponse];
+  res.json(jsonResponse)
+}
 );
 
 //* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post('/api/notes', (req, res) => {
   
-//using index.js variable
-//  const savenote = saveNote(req.body);
-
-  // attempt 1 hardcoding 
-  const newNote = {
-    title: 'testing',
-    text: '123',
-    uniqueID: uuid()
-  }
 
   const jsonNewNote = JSON.stringify(req.body);
 
+  const filename = "./db/db.json";
+  const oldFile = fs.readFileSync(filename);
+  const oldData = JSON.parse(oldFile);
+  const newData = [...oldData, req.body];
 
-fs.writeFile('./db/db.json', jsonNewNote, (err) =>
-err ? console.error(err) : console.log(`Note has been written to JSON file`))
+  fs.writeFileSync('./db/db.json', JSON.stringify(newData))
 
-res.json(jsonNewNote)
+  res.json(jsonNewNote)
 
 
 })
